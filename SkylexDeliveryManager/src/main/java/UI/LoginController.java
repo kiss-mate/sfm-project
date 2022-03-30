3e5e8fff644f20ae6e705a35c2a28e6aa6eb02de
kiss-mate <kiss.mate.deik@gmail.com>
2022-03-30 20:22:20 +0200

    Update LoginController.java
    fix

--- SkylexDeliveryManager/src/main/java/UI/LoginController.java
+++ SkylexDeliveryManager/src/main/java/UI/LoginController.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class LoginController extends Application {
+public class LoginController {
 
     @FXML
     private Button LoginButton;
