b962714af4ba54fec39ae9f672359f6e0f991408
kiss-mate <kiss.mate.deik@gmail.com>
2022-04-17 19:34:29 +0200

    login workflow

--- SkylexDeliveryManager/src/main/java/Main.java
+++ SkylexDeliveryManager/src/main/java/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import logic.ILogic;
 import logic.Logic;
@@ -16,7 +17,7 @@ import java.util.Scanner;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
         LoggingContext.getProperties(getClass().getResource("log-dev.properties").getFile());
 
         //set up dependencies
@@ -33,16 +34,33 @@ public class Main extends Application {
             // Add repos
             config.bind(IRepositoryBase.class).to(RepositoryBase.class);
             config.bind(IDriverRepository.class).to(DriverRepository.class);
+            config.bind(IUserRepository.class).to(UserRepository.class);
         });
 
         // set up the main window
-        var loader = new FXMLLoader();
-        loader.setLocation((getClass().getResource("UI/login.fxml")));
-        loader.setControllerFactory(injector::getInstance);
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-        stage.setTitle("Skylex Delivery Manager");
-        stage.setScene(scene);
-        stage.show();
+        var mainLoader = new FXMLLoader();
+        mainLoader.setLocation((getClass().getResource("UI/main.fxml")));
+        mainLoader.setControllerFactory(injector::getInstance);
+        Parent mainRoot = mainLoader.load();
+        Scene mainScene = new Scene(mainRoot);
+
+        // set up the login window
+        var loginLoader = new FXMLLoader();
+        loginLoader.setLocation(getClass().getResource("UI/login.fxml"));
+        loginLoader.setControllerFactory(injector::getInstance);
+        Parent loginRoot = loginLoader.load();
+        Scene loginScene = new Scene(loginRoot);
+
+        // show window
+        primaryStage.setTitle("Skylex Delivery Manager");
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+
+        // block main with login
+        Stage loginStage = new Stage();
+        loginStage.initModality(Modality.WINDOW_MODAL);
+        loginStage.initOwner(primaryStage);
+        loginStage.setScene(loginScene);
+        loginStage.showAndWait();
     }
 }
---
+++ SkylexDeliveryManager/src/main/java/repository/UserRepository.java
@@ -0,0 +1,36 @@
+package repository;
+
+import com.google.inject.Inject;
+import data.User;
+import org.hibernate.SessionFactory;
+
+import java.util.logging.Logger;
+
+public class UserRepository extends RepositoryBase<User> implements IUserRepository{
+    /**
+     * Creates the UserRepository object
+     *
+     * @param log            Logger object
+     * @param sessionFactory SessionFactory instance
+     * @throws NullPointerException log and sessionFactory cannot be null
+     */
+    @Inject
+    public UserRepository(Logger log, SessionFactory sessionFactory) {
+        super(log, sessionFactory);
+    }
+
+    @Override
+    public void update(int id, String username, String password) {
+        var session = _sessionFactory.openSession();
+
+        var oneUser = getById(id);
+        oneUser.setUserName(username);
+        oneUser.setPassword(password);
+
+        session.beginTransaction();
+        session.update(oneUser);
+        session.getTransaction().commit();
+
+        session.close();
+    }
+}
