5e7e5ad21b4178545289d9e200fccbf73cb25556
kiss-mate <kiss.mate.deik@gmail.com>
2022-04-18 08:43:24 +0200

    save work, unit tests remain

--- SkylexDeliveryManager/src/main/java/controller/LoginController.java
+++ SkylexDeliveryManager/src/main/java/controller/LoginController.java
@@ -1,4 +1,6 @@
 package controller;
+import com.google.inject.Inject;
+import common.exceptions.ArgumentNullException;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -6,6 +8,9 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Border;
 import javafx.stage.Stage;
+import logic.ILoginLogic;
+
+import java.util.logging.Logger;
 
 public class LoginController {
     public Label usernameLabel;
@@ -14,6 +19,15 @@ public class LoginController {
     public TextField usernameField;
     public PasswordField passwordField;
     public Button cancelButton;
+
+    private final Logger _log;
+    private final ILoginLogic _loginLogic;
+
+    @Inject
+    public LoginController(Logger log, ILoginLogic loginLogic) {
+        if ((_log = log) == null) throw new ArgumentNullException("log");
+        if ((_loginLogic = loginLogic) == null) throw new ArgumentNullException("loginLogic");
+    }
 
     public void LoginAction(ActionEvent e){
         if (usernameField.getText().equals("admin") && passwordField.getText().equals("pwd")) {
---
+++ SkylexDeliveryManager/src/main/java/logic/LoginLogic.java
@@ -0,0 +1,85 @@
+package logic;
+
+import com.google.inject.Inject;
+import common.exceptions.ArgumentNullException;
+import common.exceptions.BusinessException;
+import data.User;
+import enums.ErrorCodes;
+import repository.IDriverRepository;
+import repository.IUserRepository;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoginLogic implements ILoginLogic {
+    private final Logger _log;
+    private final IUserRepository _userRepo;
+
+    /**
+     * Creates new LoginLogic object for the project
+     * @param log Logger log
+     * @exception NullPointerException log cannot be null
+     * @exception NullPointerException userRepo cannot be null
+     */
+    @Inject
+    public LoginLogic(Logger log, IUserRepository userRepo) {
+        if ((_log = log) == null) throw new ArgumentNullException("log");
+        if ((_userRepo = userRepo) == null) throw new ArgumentNullException("userRepo");
+    }
+
+    @Override
+    public void addUser(String username, String password) {
+        validateLoginParams(username, password);
+
+        User newUser = new User();
+        newUser.setUserName(username);
+        newUser.setPassword(password);
+
+        _userRepo.insert(newUser);
+        _log.log(Level.INFO, "New User added to database " + newUser);
+    }
+
+    @Override
+    public void changeOneUser(int id, String username, String password) {
+        validateLoginParams(username, password);
+        var user = _userRepo.getById(id);
+        if (user == null)
+            throw new BusinessException("User not found in database", ErrorCodes.NOT_FOUND_IN_DB);
+
+        _userRepo.update(id, username, password);
+        _log.log(Level.INFO, "User updated: " + user);
+    }
+
+    @Override
+    public User getOneUser(int id) {
+        return _userRepo.getById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return _userRepo.getAll();
+    }
+
+    @Override
+    public void deleteUser(int id) {
+        var user = _userRepo.getById(id);
+        if (user == null)
+            throw new BusinessException("User not found in database", ErrorCodes.NOT_FOUND_IN_DB);
+
+        if (_userRepo.delete(user)) {
+            _log.log(Level.INFO, "Removed User object from database: " + user);
+        }
+
+        _log.log(Level.WARNING, "Could not remove User object from database: " + user);
+    }
+
+    private void validateLoginParams(String username, String password) {
+        if (username == null || username.isBlank() || username.isEmpty()) {
+            throw new BusinessException("Username is null or whitespace");
+        }
+        if (password == null || password.isBlank() || password.isEmpty()) {
+            throw new BusinessException("Password is null or whitespace");
+        }
+    }
+}
